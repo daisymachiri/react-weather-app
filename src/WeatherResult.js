8aca25b8daac6d2ce15b2d4969d40f40bd75a41c
daisymachiri <157162301+daisymachiri@users.noreply.github.com>
2024-07-20 01:57:06 +0200

    Initial commit

--- src/WeatherResult.js
+++ src/WeatherResult.js
@@ -1,17 +1,13 @@
 import React from "react";
 
 const WeatherResult = ({
-  city,
+  city = "Johannesburg", // Default value for city
   temperature,
   description,
   humidity,
   windSpeed,
   iconUrl,
 }) => {
-  if (!city) {
-    return null; // If city is empty or null, return nothing (null)
-  }
-
   return (
     <div>
       <h2>Weather in {city}</h2>
@@ -19,8 +15,7 @@ const WeatherResult = ({
       <p>Description: {description}</p>
       <p>Humidity: {humidity}%</p>
       <p>Wind Speed: {windSpeed} m/s</p>
-      {iconUrl && <img src={iconUrl} alt={description} />}{" "}
-      {/* Display the weather icon */}
+      {iconUrl && <img src={iconUrl} alt={description} />}
     </div>
   );
 };

f503d9d27762424a5cb56b48178b7aad6582af4a
daisymachiri <157162301+daisymachiri@users.noreply.github.com>
2024-07-29 23:15:14 +0200

    Add toggle button to App.js

--- src/App.js
+++ src/App.js
@@ -4,42 +4,59 @@ import Search from './Search';
 import WeatherResult from './WeatherResult';
 import Footer from './Footer';
 import './styles.css';
+import './WeatherResult.css';
 
 const App = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState('Johannesburg');
+  const [units, setUnits] = useState('metric');
 
   useEffect(() => {
-    const searchCity = (city) => {
-      const apiKey = 'b2a5adcct04b33178913oc335f405433';
-      const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-      axios.get(apiUrl).then((response) => {
-        setWeatherData(response.data);
-        getForecast(city);
-      });
+    const fetchWeatherData = async (city) => {
+      try {
+        const apiKey = 'b2a5adcct04b33178913oc335f405433';
+        const currentWeatherUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+        const forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+
+        const [currentWeatherResponse, forecastResponse] = await Promise.all([
+          axios.get(currentWeatherUrl),
+          axios.get(forecastUrl),
+        ]);
+
+        setWeatherData({
+          ...currentWeatherResponse.data,
+          forecast: forecastResponse.data.daily,
+        });
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
     };
 
-    const getForecast = (city) => {
-      const apiKey = 'b2a5adcct04b33178913oc335f405433';
-      const apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-      axios.get(apiUrl).then((response) => {
-        setWeatherData((prevState) => ({
-          ...prevState,
-          forecast: response.data.daily,
-        }));
-      });
-    };
+    fetchWeatherData(city);
+  }, [city, units]);
 
-    searchCity(city);
-  }, [city]);
+  const toggleUnits = () => {
+    setUnits((prevUnits) => (prevUnits === 'metric' ? 'imperial' : 'metric'));
+  };
 
   return (
     <div className="weather-app">
       <header>
         <Search setCity={setCity} />
+        <div className="btn-container">
+          <label className="switch btn-color-mode-switch">
+            <input
+              type="checkbox"
+              id="unit-toggle"
+              checked={units === 'imperial'}
+              onChange={toggleUnits}
+            />
+            <label className="btn-color-mode-switch-inner" data-off="°C" data-on="°F" htmlFor="unit-toggle"></label>
+          </label>
+        </div>
       </header>
       <main>
-        {weatherData && <WeatherResult weatherData={weatherData} />}
+        {weatherData && <WeatherResult weatherData={weatherData} units={units} />}
       </main>
       <Footer />
     </div>
